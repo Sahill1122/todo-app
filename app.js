5529fbdb6ec231c3311d3c30954eb6329b1d83a8
Sahil Chauhan <sahilc200204@gmail.com>
2026-09-23 22:23:43 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const max = prompt("Enter the max number");
+const random = Math.floor(Math.random() * max) + 1 ;
+let guess = prompt("guess the number");
+while (true) {
+    if (guess == "quit"){
+        console.log("user quit");
+        break;
+    }
+    if (guess == random) {
+        console.log("you are right! , congrats!! , random no was", random);
+        break;
+    } else {
+        console.log("your guess was wrong. please try again");
+    }
+}
